1f74976a6eed7219678b6e19beaa7eb346b16496
Kirinochka <78536944+Kirinochka@users.noreply.github.com>
2021-07-30 20:37:50 +0300

    array task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,8 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  if(arr1.length != arr2.length) return false;
+  result = arr1.every((val, i) => val == arr2[i])
 
   return result; // boolean
 }
@@ -10,6 +12,9 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter(val => val > 0)
+    .filter(val => val % 3 == 0)
+    .map(val => val * 10);
 
   return resultArr; // array
 }
