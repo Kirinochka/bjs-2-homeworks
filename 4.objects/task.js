b9d272637df28a62663aaaefb9af0e29d0769228
Kirinochka <78536944+Kirinochka@users.noreply.github.com>
2021-08-04 21:52:08 +0300

    objects task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
-}
+  this.subject = subjectName;
+};
 
 // ваш код для остальных методов
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks == undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks == undefined) {
+    this.marks = [...mark];
+  } else {
+    this.marks.push(...mark);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((acc, cur) => acc + cur, 0) / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+};
