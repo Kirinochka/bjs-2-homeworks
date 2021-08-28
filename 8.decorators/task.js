d4e35c33b3aa7fcb9b1d1915604f6440872e23ac
Kirinochka <78536944+Kirinochka@users.noreply.github.com>
2021-08-28 17:24:01 +0300

    decorators task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,63 @@
 function cachingDecoratorNew(func) {
   // Ваш код
+  let cache = [];
+  return function(...args) {
+    const hash = args.join(",");
+
+    let cacheItem = cache.find((val) => val.hash === hash);
+    if (cacheItem) {
+      console.log("Из кэша: " + cacheItem.result);
+      return "Из кэша: " + cacheItem.result;
+    } else {
+      let result = func.call(this, ...args);
+      cache.push({ hash, result });
+      if (cache.length > 5) cache.shift();
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
 }
 
-
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func, ms) {
   // Ваш код
+  let isStop = false;
+  let timeout;
+  return function (...args) {
+    if (isStop) {
+      return
+    }
+
+    func.call(this, ...args)
+    isStop = true
+    setTimeout(() => isStop = false, ms)
+  }
 }
 
-function debounceDecorator2(func) {
+function debounceDecorator2(func, ms) {
   // Ваш код
+  let isStop = false;
+  let timeout;
+  function wrapper (...args) {
+    if (isStop) {
+      return
+    }
+
+    func.call(this, ...args)
+    wrapper.count++
+    isStop = true
+    setTimeout(() => isStop = false, ms)
+  }
+  wrapper.count = 0;
+
+  return wrapper
 }
+
+const sendSignal = () => console.log("Сигнал отправлен");
+const upgradedSendSignal = debounceDecorator2(sendSignal, 2000);
+setTimeout(upgradedSendSignal);
+setTimeout(upgradedSendSignal, 300);
+setTimeout(upgradedSendSignal, 900);
+setTimeout(upgradedSendSignal, 1200);
+setTimeout(upgradedSendSignal, 2300);
+setTimeout(upgradedSendSignal, 4400);
+setTimeout(upgradedSendSignal, 4500);
