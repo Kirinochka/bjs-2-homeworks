2933434b810726df9873724da792a44b3990b19d
Kirinochka <78536944+Kirinochka@users.noreply.github.com>
2021-08-30 12:05:33 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,21 +1,20 @@
 function cachingDecoratorNew(func) {
   // Ваш код
   let cache = [];
-  return function(...args) {
+  return function (...args) {
     const hash = args.join(",");
 
     let cacheItem = cache.find((val) => val.hash === hash);
     if (cacheItem) {
       console.log("Из кэша: " + cacheItem.result);
       return "Из кэша: " + cacheItem.result;
-    } else {
-      let result = func.call(this, ...args);
-      cache.push({ hash, result });
-      if (cache.length > 5) cache.shift();
-      console.log("Вычисляем: " + result);
-      return "Вычисляем: " + result;
     }
-  }
+    let result = func.call(this, ...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) cache.shift();
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  };
 }
 
 function debounceDecoratorNew(func, ms) {
@@ -23,37 +22,43 @@ function debounceDecoratorNew(func, ms) {
   let isStop = false;
   let timeout;
   return function (...args) {
-    if (isStop) {
-      return
+    clearTimeout(timeout);
+
+    if (isStop == false) {
+      func.call(this, ...args);
+      isStop = true;
     }
 
-    func.call(this, ...args)
-    isStop = true
-    setTimeout(() => isStop = false, ms)
-  }
+    timeout = setTimeout(() => {
+      isStop = false;
+    }, ms);
+  };
 }
 
 function debounceDecorator2(func, ms) {
   // Ваш код
   let isStop = false;
   let timeout;
-  function wrapper (...args) {
-    if (isStop) {
-      return
+  function wrapper(...args) {
+    clearTimeout(timeout);
+
+    if (isStop == false) {
+      func.call(this, ...args);
+      isStop = true;
     }
 
-    func.call(this, ...args)
-    wrapper.count++
-    isStop = true
-    setTimeout(() => isStop = false, ms)
+    timeout = setTimeout(() => {
+      isStop = false;
+      wrapper.count++;
+    }, ms);
   }
   wrapper.count = 0;
 
-  return wrapper
+  return wrapper;
 }
 
 const sendSignal = () => console.log("Сигнал отправлен");
-const upgradedSendSignal = debounceDecorator2(sendSignal, 2000);
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
 setTimeout(upgradedSendSignal);
 setTimeout(upgradedSendSignal, 300);
 setTimeout(upgradedSendSignal, 900);
